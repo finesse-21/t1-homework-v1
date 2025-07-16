a0c44a4626f392d62b47d5a9966b11379cb430c9
Egor Alekseenko <21pepperforreal@gmail.com>
2025-07-16 09:00:49 +0300

    feat: add ability to run front and back simultaneously

--- src/shared/ui/Card.tsx
+++ src/shared/ui/Card.tsx
@@ -12,7 +12,7 @@ export const Card = styled.div<CardProps>`
   box-shadow: 0 8px 24px rgba(0, 0, 0, 0.06);
   box-sizing: border-box;
   width: 100%;
-  padding: ${({ padding }) => padding || '24px'};
+  padding: ${({ padding }) => padding || '32px'};
   max-width: ${({ maxWidth }) => maxWidth || 'unset'};
   display: flex;
   flex-direction: column;
